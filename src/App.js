64642f43762528b6604a1779f4c6793dc1761abf
Moises Pahati <moisespahati@gmail.com>
2023-07-20 17:45:32 +0800

    move jsx into separate components such as Navbar, Search and Main

--- src/App.js
+++ src/App.js
@@ -50,8 +50,46 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+
 export default function App() {
+    return (
+      <>
+        <NavBar />
+        <Main />
+      </>
+    );
+}
+
+function NavBar() {
   const [query, setQuery] = useState("");
+
+  return (
+    <nav className="nav-bar">
+        <div className="logo">
+          <span role="img">🍿</span>
+          <h1>usePopcorn</h1>
+        </div>
+
+        <p className="num-results">
+          Found <strong>X</strong> results
+        </p>
+      </nav>
+  );
+}
+
+function Search() {
+  return (
+    <input
+    className="search"
+    type="text"
+    placeholder="Search movies..."
+    value={query}
+    onChange={(e) => setQuery(e.target.value)}
+  />
+  );
+}
+
+function Main() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
   const [isOpen1, setIsOpen1] = useState(true);
@@ -62,25 +100,7 @@ export default function App() {
   const avgRuntime = average(watched.map((movie) => movie.runtime));
 
   return (
-    <>
-      <nav className="nav-bar">
-        <div className="logo">
-          <span role="img">🍿</span>
-          <h1>usePopcorn</h1>
-        </div>
-        <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <p className="num-results">
-          Found <strong>{movies.length}</strong> results
-        </p>
-      </nav>
-
-      <main className="main">
+    <main className="main">
         <div className="box">
           <button
             className="btn-toggle"
@@ -163,6 +183,7 @@ export default function App() {
           )}
         </div>
       </main>
-    </>
   );
 }
+
+
