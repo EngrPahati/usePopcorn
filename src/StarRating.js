945c3c9c47db5834c8745058b017eb244082e484
Moises Pahati <moisespahati@gmail.com>
2023-07-21 20:07:09 +0800

    add StarRating.propTypes

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from 'prop-types';
 
 
 const containerStyle = {
@@ -9,6 +10,16 @@ const containerStyle = {
 
 const starContainerStyle = {
     display: 'flex',
+};
+
+StarRating.propTypes = {
+    maxRating: PropTypes.number,
+    defaultRating: PropTypes.number,
+    color: PropTypes.string,
+    size: PropTypes.number,
+    messages: PropTypes.array,
+    className: PropTypes.string,
+    onSetRating: PropTypes.func,
 };
 
 
