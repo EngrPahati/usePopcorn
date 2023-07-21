ceb8c4ab0cb098754d84446f8cea61eafcf7b30c
Moises Pahati <moisespahati@gmail.com>
2023-07-21 12:07:19 +0800

    fix bugs on onSetRating is not a function

--- src/StarRating.js
+++ src/StarRating.js
@@ -13,12 +13,20 @@ const starContainerStyle = {
 
 
 
-export default function StarRating({ maxRating = 5, color='#fcc419', size=48 }) {
-    const [rating, setRating] = useState(0);
+export default function StarRating({
+    maxRating = 5,
+    color = '#fcc419',
+    size = 48,
+    messages = [],
+    defaultRating = 0,
+    onSetRating
+}) {
+    const [rating, setRating] = useState(defaultRating);
     const [tempRating, setTempRating] = useState(0);
 
     function handleRating(rating) {
         setRating(rating);
+        if (onSetRating) onSetRating(rating);
     }
 
     const textStyle = {
@@ -43,7 +51,9 @@ export default function StarRating({ maxRating = 5, color='#fcc419', size=48 }) 
                 />
             ))}
             </div>  
-            <p style={textStyle}>{tempRating || rating || ''}</p>
+            <p style={textStyle}>{messages.length === maxRating ?
+                messages[tempRating ? tempRating - 1 : rating - 1] :
+                tempRating || rating || ''}</p>
         </div>
     );
 }
