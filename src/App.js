6d71facae2e6eeed7a96d6432a2d82ab013b87ed
Moises Pahati <moisespahati@gmail.com>
2023-07-24 00:07:06 +0800

    add more details on MovieDetails component

--- src/App.js
+++ src/App.js
@@ -281,21 +281,56 @@ function Movie({ movie, onSelectMovie}) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre
+  } = movie;
+
+  console.log(title, year);
+
   useEffect(function () {
     async function getMovieDetails() {
       const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`);
       const data = await res.json();
-      console.log(data);
-
+      setMovie(data);
     }
+    getMovieDetails();
   }, []);
 
   return (
     <div className="details">
-      <button onClick={onCloseMovie} className="btn-back">
-        &larr;
-      </button>  
-      {selectedId}
+      <header>
+        <button onClick={onCloseMovie} className="btn-back">
+          &larr;
+        </button>
+        <img src={poster} alt={`Poster of ${movie}`} />
+        <div className="details-overview">
+          <h2>{title}</h2>
+          <p>{released} &bull; {runtime}</p>
+          <p>{genre}</p>
+          <p>
+            <span>⭐</span>
+            {imdbRating} IMDB rating
+          </p>
+        </div>
+      </header>
+      
+      <section>
+        <StarRating />
+        <p><em>{ plot }</em></p>
+        <p>Starring {actors}</p>
+        <p>Directed by {director}</p>
+      </section>
     </div>
   );
 }
