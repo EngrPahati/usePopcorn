4451832565c1b03d268714e37de877b85b661165
Moises Pahati <moisespahati@gmail.com>
2023-07-23 17:18:08 +0800

    add async function inside useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,10 +50,29 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = '5c97f505'
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  
+  //  useEffect using promise based function
+  // useEffect(function () {
+  //   fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+  //   .then(res => res.json())
+  //   .then(data => setMovies(data.Search))
+  // }, []);
+
+  // useEffect using async await function
+    
+  
+  useEffect(function () {
+    async function fetchMovies() {
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`);
+      const data = res.json();
+      setMovies(data.Search);
+    }
+  }, []);
 
   return (
     <>
