e89fc75a4ec50d60056d476b43bd088261658664
Moises Pahati <moisespahati@gmail.com>
2023-07-24 10:55:46 +0800

    add conditional rendering on StarRating and Add to list button

--- src/App.js
+++ src/App.js
@@ -143,6 +143,7 @@ export default function App() {
               selectedId={selectedId}
               onCloseMovie={handleCloseMovie}
               onAddWatched={handleAddWatched}
+              watched={watched}
             />
           ) : (
             <>
@@ -286,10 +287,12 @@ function Movie({ movie, onSelectMovie}) {
   );
 }
 
-function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
+function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRAting] = useState('');
+  const isWatched = watched.map(movie => movie.imdbID).includes(selectedId);
+  const watchedUserrating = watched.find(movie => movie.imdbID === selectedId)?.userRating;
 
   const {
     Title: title,
@@ -314,9 +317,8 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
       runtime: Number(runtime.split(' ').at(0)),
       userRating,
     };
-
-    onAddWatched(newWatchedMovie);
-    onCloseMovie();
+      onAddWatched(newWatchedMovie);
+      onCloseMovie();
   }
 
   useEffect(function () {
@@ -352,16 +354,25 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
       
       <section>
         <div className="rating">
-          <StarRating
-            maxRating={10}
-            size={24}
-            onSetRating={setUserRAting}    
+          {!isWatched ? (
+            <>     
+              <StarRating
+              maxRating={10}
+              size={24}
+              onSetRating={setUserRAting}    
               />
               {userRating > 0 && (
-                <button className="btn-add" onClick={handleAdd}>
-                  + Add to list
-                </button>)
+              <button className="btn-add" onClick={handleAdd}>
+                + Add to list
+              </button>)
               }
+            </>
+              ) : (
+            <p style={{
+            textAlign: 'center'
+            }}>
+                    You rated this movie as { watchedUserrating}</p>)
+          }
 
                  
         </div>
