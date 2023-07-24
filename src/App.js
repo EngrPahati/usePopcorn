7d6993919dbe7ec50ccf7ed102e78bab2fb65262
Moises Pahati <moisespahati@gmail.com>
2023-07-25 02:15:48 +0800

    extract hook into useKey custom hook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useRef} from "react";
 import StarRating from './StarRating';
 import { useMovies } from "./useMovies";
 import { useLocalStorageState } from "./useLocalStorageState";
+import { useKey } from "./useKey";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -113,27 +114,12 @@ function Logo() {
 function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
-  useEffect(function () {
-    function callback(e) {
-    //checking if the input element is already active
-      if (document.activeElement === inputEl.current) return;
-
-      if (e.code === 'Enter' || e.code === 'NumpadEnter') {
-        inputEl.current.focus();   
-        setQuery("");
-      }
-    }
-    document.addEventListener("keydown", callback);
-    return () => document.addEventListener("keydown", callback);
-    // when you include any props or state like setQuery you should put 
-    //it inside dependency array
-  }, [setQuery])
-  // useEffect(function () {
-  //   const el = document.querySelector('.search');
-  //   console.log(el);
-  //   el.focus();
-  // }, []);
-
+  useKey('Enter', function () {
+    if (document.activeElement === inputEl.current) return;
+    inputEl.current.focus();
+    setQuery("");
+  });
+  
   return (
     <input
       className="search"
@@ -279,21 +265,8 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     // setAvgRating(avgRating => (avgRating + userRating) / 2);
   }
 
-  useEffect(function () {
-    function callback(e) {
-      if (e.code === 'Escape') {
-        onCloseMovie();
-      }
-    }
-
-    document.addEventListener('keydown', callback);
-
-    return function () {
-      document.removeEventListener('keydown', callback);
-    }
-
-  }, [onCloseMovie]);
-
+  useKey('Escape', onCloseMovie);
+  
   useEffect(function () {
     async function getMovieDetails() {
       setIsLoading(true);
