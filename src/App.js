bc7b7c6fb24ca9e9d385f488d8823e9ccfdf5dc7
Moises Pahati <moisespahati@gmail.com>
2023-07-24 22:17:22 +0800

    add useRef to select input element

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef} from "react";
 import StarRating from './StarRating';
 
 const average = (arr) =>
@@ -165,7 +165,18 @@ function Logo() {
   );
 }
 
-function Search({query, setQuery}) {
+function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+
+    inputEl.current.focus();
+  }, [])
+  // useEffect(function () {
+  //   const el = document.querySelector('.search');
+  //   console.log(el);
+  //   el.focus();
+  // }, []);
 
   return (
     <input
@@ -174,6 +185,7 @@ function Search({query, setQuery}) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
